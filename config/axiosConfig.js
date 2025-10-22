078b02e5cbf632c7f6e72a4dd165e685164bde2e
Caryl Dizon <lyracdizon@gmail.com>
2025-10-22 11:48:38 +0800

    url attempt

--- config/axiosConfig.js
+++ config/axiosConfig.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-  baseURL: 'http://YOUR_DJANGO_SERVER_IP:8000/api', // e.g. http://192.168.1.10:8000/api
+  baseURL: 'http://192.168.1.10:8000/api', 
   timeout: 5000,
 });
 
